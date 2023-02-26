b5a8a26e3de51c17bb85bbe9db5636d03bf519d4
jihad-hossain1 <jihadkhan934@gmail.com>
2023-02-26 17:45:41 +0600

    enter for search section complete

--- phone-hunter/js/app.js
+++ phone-hunter/js/app.js
@@ -1,4 +1,4 @@
-const loadPhones = async(searchText,dataLimit) => {
+const loadPhones = async (searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
@@ -42,11 +42,19 @@ const displayPhones = (phones, dataLimit) => {
     // stop loader
     toggleSpinner(false);
 }
-
+// handle search button click
 document.getElementById('btn-search').addEventListener('click', function () {
     // start loader
     processSearch(10);
-    
+
+})
+
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        // code for enter
+        processSearch(10);
+    }
 })
 
 const toggleSpinner = isLoading => {
@@ -58,24 +66,24 @@ const toggleSpinner = isLoading => {
     }
 }
 
-const processSearch = (dataLimit) =>{
+const processSearch = (dataLimit) => {
     toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhones(searchText,dataLimit);
+    loadPhones(searchText, dataLimit);
 }
 
 
 // not the best way to load show all
-document.getElementById('btn-show-all').addEventListener('click',function(){
+document.getElementById('btn-show-all').addEventListener('click', function () {
     processSearch();
 })
 
-const loadPhoneDetails =async id =>{
+const loadPhoneDetails = async id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    console.log(data.data);
 }
 
 // loadPhones();
